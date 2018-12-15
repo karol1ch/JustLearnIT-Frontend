2888556550731ce4060fa8c8fb78d483503141c9
marcin holota <marcinholota1@gmail.com>
2018-12-15 18:47:48 +0100

    Practice pages added

--- src/page/Practice.js
+++ src/page/Practice.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import {Container} from "reactstrap";
+import {HashRouter, Route} from "react-router-dom";
+import PracticePage from "./PracticePage";
+import PracticeCategory from "./PracticeCategory";
 
 class Practice extends React.Component {
     render() {
         return (
-            <div>
-                <h5>Practice</h5>
-            </div>
+            <HashRouter>
+                <div>
+                    <Container>
+                        <Route exact path="/practice" component={PracticePage}/>
+                        <Route path="/practice/:category" component={PracticeCategory}/>
+                    </Container>
+                </div>
+            </HashRouter>
         );
     }
 }
