561db96382b34fdb54a13b4dcff420ee8df427ac
addme96 <addme96@gmail.com>
2018-12-16 18:39:06 +0100

    addes submits page

--- src/page/Submits.js
+++ src/page/Submits.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import {HashRouter, Route} from "react-router-dom";
+import {Container} from "reactstrap";
+import SubmitsPage from "./SubmitsPage";
+import SubmitsPageSubmitDetails from "./SubmitsPageSubmitDetails";
 
 class Submits extends React.Component {
     render() {
         return (
-            <div>
-                <h5>Submits</h5>
-            </div>
+                <HashRouter>
+                    <div>
+                        <Container>
+                            <Route exact path="/submits" component={SubmitsPage}/>
+                            <Route path="/submits/:submit" component={SubmitsPageSubmitDetails}/>
+                        </Container>
+                    </div>
+                </HashRouter>
         );
     }
 }
